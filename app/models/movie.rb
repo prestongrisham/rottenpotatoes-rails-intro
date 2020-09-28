196100e0be21e85399a5d89903a6439fbf132a40
Carl Grisham <cgrisham@citadel.edu>
2020-09-28 19:22:35 +0000

    part 2 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,17 @@
 class Movie < ActiveRecord::Base
+    
+    def self.all_ratings
+       ['G','PG','PG-13','R'] 
+    end
+    
+    def self.all_ratings_as_hash
+       {'G' => 'G', 'PG' => 'PG', 'PG-13' => 'PG-13', 'R' => 'R'} 
+    end
+    
+    # ['G', 'PG']
+    def self.with_ratings ratings
+       Movie.where(rating: ratings)
+    end
+    
+    
 end
